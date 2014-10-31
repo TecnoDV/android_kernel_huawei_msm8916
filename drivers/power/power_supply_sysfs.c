ccdf4c29d12faf1fb20f529cfdb7e6b9c8308a9e
Vidyadhar Gurram <cfries@motorola.com>
2014-10-31 10:44:04 -0500

    power_supply: Add POWER_SUPPLY_PROP_TAPER_REACHED
    This property is meant to indicate that the current sinking into the battery is less
    than the Taper current threshold.
    
    Bug: 18685976
    
    Signed-off-by: Vidyadhar Gurram <rnh786@motorola.com>
    Reviewed-by: Jiang-Li Yuan <jlyuan@motorola.com>
    Reviewed-by: Jason Knopsnyder <jasonk@motorola.com>
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
    Signed-off-by: Chris Fries <cfries@motorola.com>

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -223,6 +223,7 @@ static struct device_attribute power_supply_attrs[] = {
 #endif
 	POWER_SUPPLY_ATTR(resistance),
 	POWER_SUPPLY_ATTR(resistance_capacitive),
+	POWER_SUPPLY_ATTR(taper_reached),
 	/* Local extensions */
 	POWER_SUPPLY_ATTR(usb_hc),
 	POWER_SUPPLY_ATTR(usb_otg),
