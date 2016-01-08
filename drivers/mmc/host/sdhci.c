3165d64278653be08726d8e3206fb4d9a6758f7b
desalesouche <desalesouche@gmail.com>
2016-01-08 03:55:54 +0100

    Fix build again
    Change-Id: I30699c0acd51ab2f7b576debd1f5956c5ba97334

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -790,7 +790,7 @@ static void sdhci_adma_table_post(struct sdhci_host *host,
 	char *buffer;
 	unsigned long flags;
 	bool has_unaligned = false;
-	u32 command = SDHCI_GET_CMD(sdhci_readw(host));
+	u32 command = SDHCI_GET_CMD(sdhci_readw(host, SDHCI_COMMAND));
 
 	/*trace_mmc_adma_table_post(command, data->sg_len);*/
 
