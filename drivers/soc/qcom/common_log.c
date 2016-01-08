61d203ba195b79a19b2c661ce2f4bfa8ed13b0fc
desalesouche <desalesouche@gmail.com>
2016-01-08 06:57:34 +0100

    soc: qcom: common_log: Fix a memory leak in common_log driver
    Fix the memory leak in common_log_register_log_buf() function
    when registering log_first_idx with the memory with dump v2 driver.
    Also use kmemleak_not_leak when msm_dump_data_register() calls
    are successful to ensure that kmemleak doesn't report it as a memory
    leak.
    
    CRs-Fixed: 832905
    Change-Id: I36eaeebf821f64dd7503ec823aca3c7aec846bd0
    Signed-off-by: Prasad Sodagudi <psodagud@codeaurora.org>

--- drivers/soc/qcom/common_log.c
+++ drivers/soc/qcom/common_log.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -16,6 +16,7 @@
 #include <linux/init.h>
 #include <linux/kallsyms.h>
 #include <linux/slab.h>
+#include <linux/kmemleak.h>
 #include <soc/qcom/memory_dump.h>
 
 #define MISC_DUMP_DATA_LEN		4096
@@ -133,7 +134,9 @@ static void __init common_log_register_log_buf(void)
 		if (msm_dump_data_register(MSM_DUMP_TABLE_APPS, &entry_log_buf)) {
 			kfree(dump_data);
 			pr_err("Unable to register %d.\n", entry_log_buf.id);
-		}
+		} else
+			kmemleak_not_leak(dump_data);
+
 		if (fist_idxp) {
 			dump_data = kzalloc(sizeof(struct msm_dump_data), GFP_KERNEL);
 			if (!dump_data) {
@@ -143,8 +146,13 @@ static void __init common_log_register_log_buf(void)
 			dump_data->addr = virt_to_phys(fist_idxp);
 			entry_first_idx.id = MSM_DUMP_DATA_LOG_BUF_FIRST_IDX;
 			entry_first_idx.addr = virt_to_phys(dump_data);
-			if (msm_dump_data_register(MSM_DUMP_TABLE_APPS, &entry_first_idx))
-				pr_err("Unable to register %d.\n", entry_first_idx.id);
+			if (msm_dump_data_register(MSM_DUMP_TABLE_APPS,
+						 &entry_first_idx)) {
+				kfree(dump_data);
+				pr_err("Unable to register %d.\n",
+						entry_first_idx.id);
+			} else
+				kmemleak_not_leak(dump_data);
 		}
 	}
 }
